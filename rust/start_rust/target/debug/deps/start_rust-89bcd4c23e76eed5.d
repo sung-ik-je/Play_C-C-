20e2b1b9f0ff8bf52916fbee64c76fe0e5e442b6
sung-ik-je <ij_16@naver.com>
2024-08-29 21:09:46 +0900

    del rust target

--- rust/start_rust/target/debug/deps/start_rust-89bcd4c23e76eed5.d
+++
@@ -1,5 +0,0 @@
-/Users/seong-ikje/Desktop/sys_lan_practice/rust/start_rust/target/debug/deps/start_rust-89bcd4c23e76eed5: src/main.rs
-
-/Users/seong-ikje/Desktop/sys_lan_practice/rust/start_rust/target/debug/deps/start_rust-89bcd4c23e76eed5.d: src/main.rs
-
-src/main.rs:
--- rust/start_rust/target/debug/start_rust.d
+++
@@ -1,1 +0,0 @@
-/Users/seong-ikje/Desktop/sys_lan_practice/rust/start_rust/target/debug/start_rust: /Users/seong-ikje/Desktop/sys_lan_practice/rust/start_rust/src/main.rs
