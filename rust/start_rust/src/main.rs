9694e1dc8c65b66abb65202296a621ece0a10946
sung-ik-je <ij_16@naver.com>
2024-08-29 21:01:15 +0900

    rust format, print

--- rust/start_rust/src/main.rs
+++ rust/start_rust/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+  println!("Hello, world!");
+
+  format!("stdout string");
+  
+  let name = "ik";
+  let age = 30;
+  
+  let stdout = format!("name : {} year : {}", name, age);
+
+  println!("{}", stdout);
+  print!("same as format!, printed to the console");
+  println!("same as print!, append newline");
+
+  eprint!("smae as print, but printed to the stderr");
+  eprintln!("smae as eprint, append newline");
 }
