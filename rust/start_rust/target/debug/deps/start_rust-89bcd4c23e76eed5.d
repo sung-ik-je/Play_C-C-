520f7151127c6ab1355f40e76d25125d7a20da27
sung-ik-je <ij_16@naver.com>
2024-08-29 20:16:12 +0900

    rust hello world

---
+++ rust/start_rust/target/debug/deps/start_rust-89bcd4c23e76eed5.d
@@ -0,0 +1,5 @@
+/Users/seong-ikje/Desktop/sys_lan_practice/rust/start_rust/target/debug/deps/start_rust-89bcd4c23e76eed5: src/main.rs
+
+/Users/seong-ikje/Desktop/sys_lan_practice/rust/start_rust/target/debug/deps/start_rust-89bcd4c23e76eed5.d: src/main.rs
+
+src/main.rs:
---
+++ rust/start_rust/target/debug/start_rust.d
@@ -0,0 +1,1 @@
+/Users/seong-ikje/Desktop/sys_lan_practice/rust/start_rust/target/debug/start_rust: /Users/seong-ikje/Desktop/sys_lan_practice/rust/start_rust/src/main.rs
