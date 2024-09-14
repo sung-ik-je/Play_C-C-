bc1a35684269ac76ec745a577ecec0ecdce78a5b
sung-ik-je <ij_16@naver.com>
2024-09-14 12:38:36 +0900

    rust primitives

--- rust/start_rust/src/main.rs
+++ rust/start_rust/src/main.rs
@@ -1,5 +1,10 @@
+use std::mem;
 
 fn format_traits() {
+  println!("=================================================");
+  println!("format_traits");
+  println!("=================================================");
+  println!("=================================================");
   println!("Hello, world!");
 
   format!("stdout string");
@@ -57,7 +62,75 @@ fn format_traits() {
   #[allow(dead_code)] // disable `dead_code` which warn against unused module
   struct Structure(i32);
 }
+
+
+fn primitive_types() {
+  println!("=================================================");
+  println!("primitive_types");
+  println!("=================================================");
+  println!("=================================================");
+  // signed int : i8~128, unsigned int : u8~128
+  let s_int8: i8 = 12;
+  let s_int128: i128 = 12;
+  println!("size of i8 : {}", mem::size_of::<i8>()); // return 1byte
+  println!("size of i128 : {}", mem::size_of::<i128>()); // return 16byte
+  let u_int8: u8 = 12;
+  let u_int128: u128 = 12;
+
+  // rust is use i32 and f64 type for default
+  let a_float: f64 = 1.0;  // Regular annotation
+  // the case of addiotional specified type in value 
+  let an_integer   = 5i32; // Suffix annotation, 5 is to store to i32 type
+
+  // rust use immutable type in default
+  let true_or_false: bool = true;
+  // true_or_false = false; // impossible
+  // so if change value you must use mut keyword
+  let mut true_or_false: bool = true;
+  true_or_false = false;
+
+  println!("1e4 is {}, -2.5e-3 is {}", 1e4, -2.5e-3);
+
+  let tuple_of_tuples = ((1u8, 2u16, 2u32), (4u64, -1i8), -2i16);
+
+  // Tuples are printable.
+  println!("tuple of tuples: {:?}", tuple_of_tuples);
+
+  // array, [T; length]
+  let xs: [i32; 5] = [1, 2, 3, 4, 5];
+  let ys: [i32; 500] = [0; 500];
+
+  // analyze_slice(&ys[1 .. 4]);
+
+  /*
+  Option Enum
+    enum Option<T> {
+      Some(T), // have a specific value
+      None, // don't have a specific value
+    } 
+  
+  for func range
+    0..5 : 0~4
+    0..=5 : 0~5
+    5.. : 5~end, to infinite
+    ..5 : first~4
+    ..=5 : first~5
+  */
+  for i in 0..ys.len() {  // ys.len > xs.len, so after i = 5 func cout too far
+    match xs.get(i) { // match func is the roll of Option type matching
+      // if i > xs's index range, return None
+      Some(xval) => println!("{}: {}", i, xval),
+      None => println!("Slow down! {} is too far!", i),
+    }
+  }
+
+  let empty_array: [u32; 0] = [];
+  assert_eq!(&empty_array, &[]);
+}
+
 fn main() {
 
   format_traits();
+
+  primitive_types();
 }
